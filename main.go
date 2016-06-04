85ca5ab18792f069e6487d25a494e9a243e9bc9e
tkido <takanorikido@gmail.com>
2016-06-04 23:11:56 +0900

    Read initials.txt

--- main.go
+++ main.go
@@ -1,9 +1,32 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"os"
 )
 
 func main() {
-	fmt.Println("Hello nicoime!!")
+	lines := readLines("initials.txt")
+	fmt.Printf("lines: %v\n", lines)
+}
+
+func readLines(path string) []string {
+	f, err := os.Open(path)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "File \"%s\" could not read: %v\n", path, err)
+		os.Exit(1)
+	}
+	defer f.Close()
+
+	lines := make([]string, 0, 100)
+	scanner := bufio.NewScanner(f)
+	for scanner.Scan() {
+		lines = append(lines, scanner.Text())
+	}
+	if serr := scanner.Err(); serr != nil {
+		fmt.Fprintf(os.Stderr, "File %s scan error: %v\n", path, serr)
+	}
+
+	return lines
 }
